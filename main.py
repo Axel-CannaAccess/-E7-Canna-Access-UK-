0e0ef56ff28f59e1e97c5b3b030c56985cb48cfc
Axel-CannaAccess <axelmk03@icloud.com>
2024-08-30 23:29:36 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+from langchain.retrievers.you import YouRetriever
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+
+
+os.environ["YDC_API_KEY"] = "YOUR YOU.COM API KEY"
+os.environ["OPENAI_API_KEY"] = "YOUR OPENAI API KEY"
+yr = YouRetriever()
+model = "gpt-3.5-turbo-16k"
+qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model=model), chain_type="stuff", retriever=yr)
