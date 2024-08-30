3b2f1e2616eba3cc40a6e4868f5efc48f56522b0
Axel-CannaAccess <axelmk03@icloud.com>
2024-08-30 23:35:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,36 @@ qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model=model), chain_type="stuff"
 
 qa.run("how was the New York City pinball ban lifted?")
 
+import requests
+
+def get_ai_snippets_for_query(query):
+    headers = {"X-API-Key": os.environ["AUTH_KEY"]}
+    results = requests.get(
+        f"https://api.ydc-index.io/search?query={query}",
+        headers=headers,
+    ).json()
+
+    # We return many text snippets for each search hit so
+    # we need to explode both levels
+    return "\n".join(["\n".join(hit["snippets"]) for hit in results["hits"]])
+
+
+def get_cohere_prompt(query, context):
+    return f"""given a question and a bunch of snippets context try to answer the question using the context. If you can't please say 'Sorry hooman, no dice'.
+question: {query}
+context: {context}
+answer: """
+
+def ask_cohere(query, context):
+    try:
+        return cohere_client.generate(prompt=get_cohere_prompt(query, context))[
+            0
+        ].text
+    except Exception as e:
+        print(
+            "Cohere call failed for query {} and context {}".format(query, context)
+        )
+        print(e)
+        return "Sorry hooman, no dice"
+
+
